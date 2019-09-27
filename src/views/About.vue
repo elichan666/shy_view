b2fbafb65030497f756a4e69a5846d272f463fa6
elichan666 <1012178963@qq.com>
2019-09-27 18:58:23 +0800

    配置了vue.config.js 跨域、echarts、封装axios

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,86 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="getEchartData">点我</button>
+     <div id="myChart" style="width:100%;height:800px"></div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+  import https from '@/https.js';
+  import echarts from 'echarts'
+  export default {
+    name: 'home',
+    components: {
+    },
+    mounted(){
+      let arr=this.dir.split('\\');
+      this.cname=arr[arr.length-1];
+    },
+    data(){
+      return {
+        dir:'C:\\Users\\Administrator\\Desktop\\shy_pm\\dist',
+        cname:'',
+      }
+    },
+    methods:{
+      getEchartData(){
+        let params={
+          dir:this.dir
+        }
+        https.fetchPost('/getConstruction',params).then((res)=>{
+          console.log(res);
+          let obj={
+            name:this.cname,
+            children:res.data
+          }
+          const myChart = echarts.init(document.getElementById('myChart'));
+          const option = {
+            series : [
+              {
+                type: 'tree',
+
+                data: [obj],
+
+                top: '1%',
+                left: '7%',
+                bottom: '1%',
+                right: '20%',
+
+                symbolSize: 7,
+
+                label: {
+                  normal: {
+                    position: 'left',
+                    verticalAlign: 'middle',
+                    align: 'right',
+                    fontSize: 12
+                  }
+                },
+
+                leaves: {
+                  label: {
+                    normal: {
+                      position: 'right',
+                      verticalAlign: 'middle',
+                      align: 'left'
+                    }
+                  }
+                },
+
+                expandAndCollapse: true,
+                animationDuration: 550,
+                animationDurationUpdate: 750
+              }
+            ]
+          }
+          myChart.setOption(option);
+          window.addEventListener('resize', function () {
+            myChart.resize()
+          })
+        })
+      },
+    
+    }
+  }
+</script>
